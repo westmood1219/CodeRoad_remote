a3a07656a51c8463a72d1c441f87e3c3e2baa016
westmood1219 <156566837+westmood1219@users.noreply.github.com>
2024-12-05 01:37:58 +0800

    文件夹整理分类了一下

---
+++ operator_intermediate/操作符练习.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+//统计二进制中1的个数题目内容：
+//写一个函数返回参数neicu二进制中1的个数
+//比如：15  0000 1111  4个1
+//int numof1(int a)
+//{
+//    int count = 0;
+//    while (a)
+//    {
+//        if ((a % 2) != 0)
+//        {
+//            count++;
+//        }
+//        a /= 2;
+//    }
+//    return count;
+//}//一般写法
+//int numof1(int a)
+//{
+//    int count = 0;
+//    for(int i = 0;i<32;i++)
+//    {
+//        if (a & 1 == 1)
+//        {
+//            count++;
+//        }
+//            a >>= 1;
+//            //or a>>i
+//    }
+//    return count;
+//}//学了操作符的写法;
+int numof1(int n)
+{
+    int count = 0;
+    while (n)
+    {
+        n = n & (n - 1);
+        count++;
+    }
+    return count;
+}//面试写法
+
+int main()
+{
+    int input = 0;
+    scanf_s("%d", &input);
+    printf("%d个1", numof1(input));
+    return 0;
+}
