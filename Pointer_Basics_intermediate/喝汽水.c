204c33eff1895c793a3c78998eb461d322c392cc
westmood1219 <git@github.com:westmood1219>
2024-12-07 00:28:03 +0800

    test

--- Pointer_Basics_intermediate/喝汽水.c
+++ Pointer_Basics_intermediate/喝汽水.c
@@ -1,33 +1,50 @@
-//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）
+////喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）
+//
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int money = 20;
+//    int bottle = money;
+//    int empty = bottle;
+//    while (empty)
+//    {
+//        bottle += (empty / 2);
+//        if (empty == 2)
+//        {
+//            break;
+//        }
+//        else if (empty == 1)
+//        {
+//            break;
+//        }
+//        else if (empty % 2 == 1)
+//        {
+//            empty = empty / 2 + 1;
+//        }
+//        else if(empty%2 ==0)
+//        {
+//            empty /= 2;
+//        }
+//    }
+//    printf("%d", bottle);
+//    return 0;
+//}
+//状态不好,写的很垃圾
 
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int money = 20;
-    int bottle = money;
-    int empty = bottle;
-    while (empty)
-    {
-        bottle += (empty / 2);
-        if (empty == 2)
-        {
-            bottle += 1;
-            empty = 0;
-        }
-        else if (empty == 1)
-        {
-            break;
-        }
-        else if (empty % 2 == 1)
-        {
-            empty = empty / 2 + 1;
-        }
-        else if(empty%2 ==0)
-        {
-            empty /= 2;
-        }
+    int bottle = money; // 用钱买汽水，得到相同数量的空瓶
+    int empty = bottle; // 初始空瓶数
+
+    while (empty >= 2) {
+        int exchange = empty / 2; // 可以换多少瓶汽水
+        bottle += exchange; // 增加总汽水数
+        empty = exchange + (empty % 2); // 换回来的空瓶加上剩余的空瓶
     }
-    printf("%d", bottle);
+
+    printf("%d\n", bottle);
     return 0;
 }
