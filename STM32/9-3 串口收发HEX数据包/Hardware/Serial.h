4b5a341c952362d3f0b089bcb32efff0ab1bfbae
westmood1219 <156566837+westmood1219@users.noreply.github.com>
2025-04-23 18:41:47 +0800

    发送/接收HEX/文本数据包

---
+++ STM32/9-3 串口收发HEX数据包/Hardware/Serial.h
@@ -0,0 +1,20 @@
+#ifndef __SERIAL_H
+#define __SERIAL_H
+
+#include <stdio.h>
+
+extern uint8_t Serial_TxPacket[];
+extern uint8_t Serial_RxPacket[];
+
+void Serial_Init(void);
+void Serial_SendByte(uint8_t Byte);
+void Serial_SendArray(uint8_t *Array, uint16_t Length);
+void Serial_SendString(char *String);
+uint32_t Serial_Pow(uint32_t X, uint32_t Y);
+void Serial_SendNumber(uint32_t Number, uint8_t Length);
+void Serial_Printf(char *format, ...);
+uint8_t Serial_GetRxFlag(void);
+void Serial_SendPacket(void);
+
+	
+#endif
